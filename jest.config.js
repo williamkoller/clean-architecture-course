e8c9acf257a18a2719c35505e149efc271f0bcf3
William Koller <williamkoller30@gmail.com>
2024-11-07 17:00:58 -0300

    chore: add configs husky, eslint, tsconfig and jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    preset: '@shelf/jest-mongodb',
+    roots: ['<rootDir>/test'],
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!**/test/**',
+        '!**/config/**'
+    ],
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@test/(.*)$': '<rootDir>/test/$1'
+    },
+    setupFiles: ['dotenv/config']
+}
